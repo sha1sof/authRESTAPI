4c1fb7aed645b05012bf69a9992beb659019c730
Ilya <shaisof@yandex.ru>
2024-07-10 23:45:32 +0400

    Added and configured the logger

--- cmd/authServer/main.go
+++ cmd/authServer/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"github.com/sha1sof/authRESTAPI/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoadConfig()
-	fmt.Println(cfg)
 
-	//TODO: init logger
+	log := setupLogger(cfg.Env)
+	log.Info("starting logger",
+		slog.String("env", cfg.Env))
 
 	//TODO: init storage
 
 	//TODO: init server
+}
+
+// setupLogger initializes and returns a logger based on the environment.
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		panic("invalid env")
+	}
+	return logger
 }
